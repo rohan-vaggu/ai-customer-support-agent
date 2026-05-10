1f38c6c2dd794cec622e03194d64e74ccc2bc0d3
Rohan <rohanvaggu2416@gmail.com>
2026-05-10 08:36:58 +0000

    Modify env

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,13 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-customer-support-agent-psi.vercel.app/"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "https://ai-customer-support-agent-psi.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
