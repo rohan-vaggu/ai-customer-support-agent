7ad90854f667a28a8906454fffaf3a0aa8d39b43
Rohan <rohanvaggu2416@gmail.com>
2026-05-10 16:01:33 +0000

    fix backend

--- backend/app.py
+++ backend/app.py
@@ -26,9 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://ai-customer-support-agent-psi.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
